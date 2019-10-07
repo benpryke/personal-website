3b4ba497ba103a742c0732c1e835e8b5715e1878
Ben Pryke <ninjakannon@hotmail.com>
2019-10-07 09:25:36 +0100

    Patch prod-only hero rehydration bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,48 +26,55 @@ export default class Hero extends React.Component<HeroProps, HeroState> {
   static MIN_ANIM_WINDOW_WIDTH = 900;
   static MIN_ANIM_WINDOW_HEIGHT = 700;
 
+  readonly state:HeroState = {
+    height: Hero.computeHeight(),
+  };
+
   constructor(props: HeroProps) {
     super(props);
-
-    this.state = {
-      height: Hero.MAX_HEIGHT,
-    }
-    
-    this.computeHeight = this.computeHeight.bind(this);
+    this.setHeight = this.setHeight.bind(this);
   }
-
+  
   componentDidMount() {
-    document.addEventListener('scroll', this.computeHeight);
-    window.addEventListener('resize', this.computeHeight);
+    document.addEventListener('scroll', this.setHeight);
+    window.addEventListener('resize', this.setHeight);
+
+    // As we use react-snap and ReactDOM.hydrate in prod, if the user refreshes
+    // the page while scrolled down the .hero-content max-height will mismatch.
+    // This effectively freezes us in a broken state unless we update height.
+    // We can fix this with the following double-render hack.
+    if (window.scrollY > 0) {
+      this.setState({ height: this.state.height + 0.0001 });
+    }
   }
 
   componentWillUnmount() {
-    document.removeEventListener('scroll', this.computeHeight);
-    window.removeEventListener('resize', this.computeHeight);
+    document.removeEventListener('scroll', this.setHeight);
+    window.removeEventListener('resize', this.setHeight);
   }
 
-  computeHeight(): void {
-    const top = window.scrollY;
-    
+  static computeHeight(): number {
     // Match the min-width and min-height media rules for applying a header animation
     if (window.innerWidth < Hero.MIN_ANIM_WINDOW_WIDTH || window.innerHeight < Hero.MIN_ANIM_WINDOW_HEIGHT) {
-      this.setState({
-        height: Hero.MAX_HEIGHT,
-      });
+      return Hero.MAX_HEIGHT;
     } else {
-      this.setState({
-        height: top < Hero.SCROLL_CUTOFF ? Hero.MAX_HEIGHT - top : Hero.MAX_HEIGHT - Hero.SCROLL_CUTOFF,
-      });
+      const top = window.scrollY;
+      return top < Hero.SCROLL_CUTOFF ? Hero.MAX_HEIGHT - top : Hero.MAX_HEIGHT - Hero.SCROLL_CUTOFF;
     }
+  }
+
+  setHeight(): void {
+    this.setState({ height: Hero.computeHeight() });
   }
 
   render() {
     const { name, headshot, ...rest } = this.props;
-    const headshotSize = this.state.height * 0.8;
+    const { height } = this.state;
+    const headshotSize = height * 0.8;
 
     return (
       <Banner className='hero' {...rest}>
-        <div className='hero-content' style={{ maxHeight: this.state.height }}>
+        <div className='hero-content' style={{ maxHeight: height }}>
           <img src={headshot} alt={name} style={{
             width: headshotSize,
             height: headshotSize,
