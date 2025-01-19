38f50f9057817b127d6cbd9c3472ee41bd8cc9c5
Ben Pryke <ninjakannon@hotmail.com>
2025-01-19 18:36:53 +0000

    chore: filter CSS parse errors from test output

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,18 @@
 import { vi } from "vitest";
 
+// Filter CSS parse errors from test output
+const consoleErrorFn = console.error;
+console.error = (...data) => {
+  if (
+    typeof data[0]?.toString === "function" &&
+    data[0].toString().includes("Error: Could not parse CSS stylesheet")
+  ) {
+    return;
+  }
+
+  consoleErrorFn(...data);
+};
+
 // IntersectionObserver isn't available in test environment
 beforeEach(() => {
   const mockIntersectionObserver = vi.fn();
